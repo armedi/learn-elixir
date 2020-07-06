31ea6f0194ef05b41e0c7b8fe12c7b22d5255545
Armedi <armedi@outlook.com>
2020-07-06 17:17:56 +0700

    completed dictionary module

---
+++ dictionary/lib/dictionary.ex
@@ -0,0 +1,14 @@
+defmodule Dictionary do
+  @spec random_word :: binary
+  def random_word do
+    words_list()
+    |> Enum.random()
+  end
+
+  @spec words_list :: [binary]
+  def words_list do
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+end
